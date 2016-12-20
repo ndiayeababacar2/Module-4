f73c44aba4aeaf9d8c29999e5679ec8ea0822a69
ndiayeababacar2 <ndiaye.ababacar2@yahoo.com>
2016-12-20 13:12:59 -0600

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -2,18 +2,15 @@
 
 var helloSpeaker = {};
   
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = "Paula"
 
 var speakWord ="hello";
-
-  
-  for (var i = 0; i < names.length; i++) {
   
 helloSpeaker.speak = function(names) {
 
 console.log(speakWord + " " + names);
 
-}}
+}
 
 window.helloSpeaker=helloSpeaker;
 
